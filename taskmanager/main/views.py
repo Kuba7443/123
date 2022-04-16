f29b140d729818049155a587d52012c48a11787d
user <kubanych.zh.u@gmail.com>
2022-04-16 14:32:42 +0600

    1

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title':'Home page', 'tasks': tasks})
+
 def about(request):
     return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'The form was wrong'
+
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
